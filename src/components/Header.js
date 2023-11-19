40c91c1f87a55b7cbe56dcf3c8e2ecae75159fb7
ElenaK <helenochka4@gmail.com>
2023-11-19 16:04:25 +0100

    component Navbar

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { Button, Container, Form, FormControl, Navbar,Nav} from 'react-bootstrap'
+import logo from './logo192.png'
+export default class Header extends Component {
+  render() {
+    return (
+      <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              src={logo}
+              height="30"
+              width="30"
+              className="d-inline-block-align-top"
+              alt="Logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav" />
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/blog">Blog</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Container>
+      </Navbar>
+    );
+  }
+}
