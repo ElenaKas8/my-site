c79e86c1c9ae12a125c83c9331e6ebd1f4e1d191
ElenaK <helenochka4@gmail.com>
2023-11-20 20:31:56 +0100

    blog

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,11 +1,112 @@
-import React, { Component } from 'react'
-import CarouselBox from "../components/CarouselBox";
-
-
+import React, { Component } from "react";
+import { Container, Row, Col, Card, ListGroup } from "react-bootstrap";
 export default class Blog extends Component {
   render() {
     return (
-      <CarouselBox />
-    )
+      <Container>
+        <Row>
+          <Col md="9">
+            <div className="media m-5">
+              <img
+                width={150}
+                height={150}
+                className="mr-3"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEzUDGBtRyfAaDy1U41RwSGKPOUSFvhXvvwGtXsBU0bQ&s"
+              />
+              <div className="media-body">
+                <h5>Blog Post</h5>
+                <p>
+                  "Adventures in the world of technology: artificial
+                  intelligence, virtual reality, and cybersecurity. Society's
+                  evolution in the age of digitization. New horizons and
+                  challenges awaiting us in the future."
+                </p>
+              </div>
+            </div>
+
+            <div className="media m-5">
+              <img
+                width={150}
+                height={150}
+                className="mr-3"
+                src="https://encrypted-tbn0.gstatic.com/images?
+q=tbn:ANd9GcQEzUDGBtRyfAaDy1U41RwSGKPOUSFvhXvvwGtXsBU0bQ&s"
+              />
+              <div className="media-body">
+                <h5>Blog Post</h5>
+                <p>
+                  "Adventures in the world of technology: artificial
+                  intelligence, virtual reality, and cybersecurity. Society's
+                  evolution in the age of digitization. New horizons and
+                  challenges awaiting us in the future."
+                </p>
+              </div>
+            </div>
+
+            <div className="media m-5">
+              <img
+                width={150}
+                height={150}
+                className="mr-3"
+                src="https://encrypted-tbn0.gstatic.com/images?
+q=tbn:ANd9GcQEzUDGBtRyfAaDy1U41RwSGKPOUSFvhXvvwGtXsBU0bQ&s"
+              />
+              <div className="media-body">
+                <h5>Blog Post</h5>
+                <p>
+                  "Adventures in the world of technology: artificial
+                  intelligence, virtual reality, and cybersecurity. Society's
+                  evolution in the age of digitization. New horizons and
+                  challenges awaiting us in the future."
+                </p>
+              </div>
+            </div>
+
+            <div className="media m-5">
+              <img
+                width={150}
+                height={150}
+                className="mr-3"
+                src="https://encrypted-tbn0.gstatic.com/images?
+q=tbn:ANd9GcQEzUDGBtRyfAaDy1U41RwSGKPOUSFvhXvvwGtXsBU0bQ&s"
+              />
+              <div className="media-body">
+                <h5>Blog Post</h5>
+                <p>
+                  "Adventures in the world of technology: artificial
+                  intelligence, virtual reality, and cybersecurity. Society's
+                  evolution in the age of digitization. New horizons and
+                  challenges awaiting us in the future."
+                </p>
+              </div>
+            </div>
+          </Col>
+          <Col md="3">
+            <h5 className="text-center mt=5">Categories</h5>
+            <Card>
+              <ListGroup variant="slush">
+                <ListGroup.Item>Html/Css</ListGroup.Item>
+                <ListGroup.Item>JavaScript</ListGroup.Item>
+                <ListGroup.Item>Python</ListGroup.Item>
+                <ListGroup.Item>Java</ListGroup.Item>
+                <ListGroup.Item>C++</ListGroup.Item>
+              </ListGroup>
+            </Card>
+
+            <Card className="mt-3" bg="light">
+              <Card.Body>
+                <Card.Title>Side widget</Card.Title>
+                <Card.Text>
+                  "Adventures in the world of technology: artificial
+                  intelligence, virtual reality, and cybersecurity. Society's
+                  evolution in the age of digitization. New horizons and
+                  challenges awaiting us in the future."
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import CarouselBox from '../components/CarouselBox';
-import { Container, Card,Button, CardGroup } from "react-bootstrap";
+import React, { Component } from "react";
+import CarouselBox from "../components/CarouselBox";
+import { Container, Card, Button, CardGroup } from "react-bootstrap";
 
 export default class Home extends Component {
   render() {
@@ -9,11 +9,11 @@ export default class Home extends Component {
         <CarouselBox />
         <Container>
           <h2 className="text-center mr-4">Our team</h2>
-          <CardGroup>
-            <Card>
+          <CardGroup className="m-4">
+            <Card border="primary">
               <Card.Img
                 variant="top"
-                src="https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=600" width={300}
+                src="https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=600"
               />
               <Card.Body>
                 <Card.Title>Developers</Card.Title>
@@ -24,14 +24,8 @@ export default class Home extends Component {
                 <Button variant="primary">About team</Button>
               </Card.Body>
             </Card>
-          </CardGroup>
 
-          <CardGroup>
-            <Card>
-              <Card.Img
-                variant="top"
-                src="https://images.pexels.com/photos/3184423/pexels-photo-3184423.jpeg?auto=compress&cs=tinysrgb&w=600"
-              />
+            <Card border="primary">
               <Card.Body>
                 <Card.Title>Developers</Card.Title>
                 <Card.Text>
@@ -40,11 +34,14 @@ export default class Home extends Component {
                 </Card.Text>
                 <Button variant="primary">About team</Button>
               </Card.Body>
+              <Card.Img
+                variant="bottom"
+                src="https://images.pexels.com/photos/3184423/pexels-photo-3184423.jpeg?auto=compress&
+                cs=tinysrgb&w=600"
+              />
             </Card>
-          </CardGroup>
 
-          <CardGroup>
-            <Card>
+            <Card border="primary">
               <Card.Img
                 variant="top"
                 src="https://images.pexels.com/photos/3184421/pexels-photo-3184421.jpeg?auto=compress&cs=tinysrgb&w=600"
@@ -61,23 +58,6 @@ export default class Home extends Component {
           </CardGroup>
         </Container>
       </>
-    );       
-     } 
+    );
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
